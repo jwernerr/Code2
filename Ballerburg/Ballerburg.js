175591e0b56fdd09495d4de85720271670b007df
jenny <jenny.w0400@gmail.com>
2024-10-22 10:34:58 +0200

    ballerburgrip

--- Ballerburg/Ballerburg.js
+++ Ballerburg/Ballerburg.js
@@ -1,2 +1,2 @@
 "use strict";
-console.log("test");
+//interfaces&variables
--- Ballerburg/Ballerburg.ts
+++ Ballerburg/Ballerburg.ts
@@ -1,1 +1,15 @@
-console.log("test");
+//interfaces&variables
+
+interface Cannon{
+    index:number,
+    angle:number,
+    gunpowder:number,
+    ballinair:boolean
+}
+
+interface Ball{
+    oldPosX:number,
+    newPosX:number,
+    oldPosY:number,
+    newPosY:number
+}
