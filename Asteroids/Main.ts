89afd2491d62c6eadb1b051b34d802e5922aec59
jenny <jenny.w0400@gmail.com>
2024-10-28 20:48:21 +0100

    created Asteroids

---
+++ Asteroids/Main.ts
@@ -0,0 +1,17 @@
+window.addEventListener("load", handleLoad);
+
+export let crc2: CanvasRenderingContext2D;
+
+function handleLoad(_event: Event): void {
+    console.log("asteroids starting");
+    let canvas: HTMLCanvasElement = document.querySelector("canvas")!;
+    if (!canvas) {
+        return;
+    }
+    crc2 = <CanvasRenderingContext2D>canvas.getContext("2d");
+    crc2.fillStyle = "black";
+    crc2.strokeStyle = "white";
+
+    createPaths();
+    console.log("Asteroid paths: ", asteroidPaths);
+}
