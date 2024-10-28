0eff0be54d450e5abea71ac7eab991af172b6492
jenny <jenny.w0400@gmail.com>
2024-10-28 20:48:44 +0100

    js

--- Asteroids/Main.js
+++ Asteroids/Main.js
@@ -1,1 +1,16 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.crc2 = void 0;
+window.addEventListener("load", handleLoad);
+function handleLoad(_event) {
+    console.log("asteroids starting");
+    let canvas = document.querySelector("canvas");
+    if (!canvas) {
+        return;
+    }
+    exports.crc2 = canvas.getContext("2d");
+    exports.crc2.fillStyle = "black";
+    exports.crc2.strokeStyle = "white";
+    createPaths();
+    console.log("Asteroid paths: ", asteroidPaths);
+}
